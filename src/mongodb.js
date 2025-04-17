22683beca4862863b10f3f58e217f667643f8f02
jp4950 <128890385+jp4950@users.noreply.github.com>
2025-04-17 14:20:19 +0530

    Update mongodb.js

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Hotel_Reservation")
+mongoose.connect("mongodb://127.0.0.1:27017/Hotel_Reservation")
 .then(() => {
     console.log("✅ MongoDB is connected");
 })
